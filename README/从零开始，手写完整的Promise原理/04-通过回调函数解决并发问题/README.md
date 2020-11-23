ff4cbd11d7fc6f8997c1f2e7c6ac46ec8c8ca31b
Jean <cingzion@gmail.com>
2020-11-23 23:46:34 +0800

    04-通过回调函数解决并发问题

---
+++ README/从零开始，手写完整的Promise原理/04-通过回调函数解决并发问题/README.md
@@ -0,0 +1,60 @@
+#### 04-通过回调函数解决并发问题
+```ts
+    // age.txt
+        12
+    // name.txt
+        jean
+
+    
+    // index.ts
+    // 04-通过回调函数解决并发问题
+    /**
+     * Promise 最重要的核心，就是解决了，
+     *  1、异步并发问题
+     *  2、回调地域问题，回调函数
+     *
+     *
+     */
+    
+    const fs = require('fs'); // 可以读取文件
+    
+    // 接口
+    interface IPerson {
+        age: number;
+        name: string;
+    }
+    
+    
+    function after(times, callback) { // 高阶函数，可以暂存变量
+        /**
+         * times 是时间
+         * callback 是一个回调函数
+         */
+        let obj = {} as IPerson;
+        return function(key: string, val: string | number){
+            /**
+             * key 是 fn() 里面的第一个参数
+             * val 是 fn() 里面的第二个参数
+             */
+            obj[key] = val;
+            --times === 0 && callback(obj);
+        }
+    }
+    
+    let fn = after(2, (obj) => {
+        console.log(obj);
+    });
+    
+    fs.readFile('./age.txt', 'utf8', (err, data): any => {
+        console.log("打印的结果是：", data);
+        fn('age', data); // 调用 fn 的函数
+    });
+    
+    
+    fs.readFile('./name.txt', 'utf8', (err, data): any => {
+        console.log("打印的结果是: ",data);
+        fn('name', data); // 调用 fn 的函数
+    });
+    
+    // 发布订阅模式 => 观察者模式
+```
---
+++ README/从零开始，手写完整的Promise原理/04-通过回调函数解决并发问题/promise-ts/deist/bundle.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// 柯里化函数
+var __spreadArrays = (undefined && undefined.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+// 创建一个函数方法，做为类型的判断处理使用
+/**
+ * 我们借用了高阶函数，让一个大的范围函数，变成了一个范围小的函数，
+ * 柯里化的功能 ，就是让函数的功能更具体些, 它的核心就是(保留参数)
+ *     是把一个N个参数的函数，拆成了分布执行的函数。
+ * 返柯里化，就是让函数范围变大 let toString Object.prototype.toString => toString.call(xxx);
+ *
+ * @param typing
+ */
+/*
+function isType(typing: string) { // 高阶函数可以用于保存参数
+    // 返回一个函数
+    return function (val: unknown) {
+        console.log("返回一个函数: ",Object.prototype.toString.call(val));
+        // 返回一个函数类型字符:  [object String]
+        // 返回一个函数类型字符:  [object Number]
+        return Object.prototype.toString.call(val) === `[object ${typing}]`;
+    }
+}
+
+// 声明一个数组
+['String', 'Number', 'Boolean'].forEach(type => {
+    // 使用 utils
+    utils[`is${type}`] = isType(type); // 这里就是所谓的叫闭包
+});
+
+console.log("isString 输出的结果是：",utils.isString('123'));
+console.log("isNumber 输出的结果是：", utils.isNumber(123));
+*/
+/***********************************************************
+ * 第三版：柯里化函数
+ **********************************************************/
+/**
+ * 实现一个通用的柯里化函数, 可以自动的将一个函数转换成多次传递参数
+ * @param typing
+ * @param val
+ */
+var curring = function (fn) {
+    // 用数组记录传递过来的参数
+    var exec = function (sumArgs) {
+        // 如何当前传入参数的个数小于函数的参数个数，需要返回一个新的函数，并且保留当前传入的参数
+        console.log("args: ", sumArgs);
+        /*
+        args:  []
+        args:  [ 1 ]
+        args:  []
+        args:  [ 1 ]
+        args:  [ 1, 2, 3 ]
+        args:  [ 1, 2, 3, 4 ]
+        -----------------------------
+        args:  [ 1, 2, 3 ]
+        args:  [ 1, 2, 3, 4 ]
+         */
+        return sumArgs.length >= fn.length ? fn.apply(void 0, sumArgs) : function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            return exec(__spreadArrays(sumArgs, args));
+        };
+    };
+    return exec([]); // 用于收集每次执行时传入的参数，第一次默认就是空的
+};
+/**
+ * 一般做柯里，都是在参数固定的情况下做柯里化, 函数的长度就是参数的个数
+ * @param a
+ * @param b
+ * @param c
+ * @param d
+ */
+function sum(a, b, c, d) {
+    return a + b + c + d;
+}
+var fn = curring(sum);
+fn = fn(1);
+var c = curring(sum)(1)(2, 3)(4);
+console.log("curring 打印结果是：", c); // 打印结果是： 10
+console.log("fn 打印结果是：", fn(2, 3)(4)); // 打印结果是： 10
+function isType(typing, val) {
+    return Object.prototype.toString.call(val) === "[object " + typing + "]";
+}
+var isString = curring(isType)('string');
+var isNumber = curring(isType)('Number');
+// 暂存变量
+console.log("isString 打印结果是", isString); // isString 打印结果是 [Function (anonymous)]
+console.log("isNumber 打印结果是", isNumber); // isNumber 打印结果是 [Function (anonymous)]
